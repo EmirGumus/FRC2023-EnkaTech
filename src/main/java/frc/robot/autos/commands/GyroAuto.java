67f6670b896fc890ef1022fab17b8e20e61d8d2c
Bw4gg <74255228+BlueWolf2GG@users.noreply.github.com>
2023-03-25 16:22:56 +0300

    Update GyroAuto.java

--- src/main/java/frc/robot/autos/commands/GyroAuto.java
+++ src/main/java/frc/robot/autos/commands/GyroAuto.java
@@ -19,25 +19,21 @@ public class GyroAuto extends CommandBase{
 
     @Override
     public void execute() {
-        if(gyro.getPitch()>5){
-            while(gyro.getPitch()>5){
-                m_speed = 0.35;
+            while(gyro.getRoll()>5){
+                m_speed = 0.2;
                 motor.mSwerveMods[0].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[1].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[2].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[3].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
             }
-        }
-        if(gyro.getPitch()<-5){
-            while(gyro.getPitch()<-5){
-                m_speed = -0.35;
+            while(gyro.getRoll()<-5){
+                m_speed = -0.2;
                 motor.mSwerveMods[0].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[1].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[2].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
                 motor.mSwerveMods[3].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
-            }    
-        }
-        if(gyro.getPitch()<=5 && gyro.getPitch()>=-5){
+            }
+        if(gyro.getRoll()<4 && gyro.getRoll()>-4){
             m_speed = 0;
             motor.mSwerveMods[0].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
             motor.mSwerveMods[1].mDriveMotor.set(ControlMode.PercentOutput, m_speed);
